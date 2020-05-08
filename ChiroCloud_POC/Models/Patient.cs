1a7d56553a39e2cf8027a3b64c71062dcd1081d1
Rudy Franco <rfranco@forteholdings.com>
2020-05-08 14:39:00 -0400

    Working POC

---
+++ ChiroCloud_POC/Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChiroCloud_POC.Models
+{
+    public partial class Patient
+    {
+        public int Id { get; set; }
+        public int? EmployerId { get; set; }
+        public string NameFirst { get; set; }
+        public string NameLast { get; set; }
+        public string NameMiddle { get; set; }
+        public string NamePreferred { get; set; }
+        public string NamePrefix { get; set; }
+        public string NameSuffix { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Sex { get; set; }
+        public string Gender { get; set; }
+        public string SocialSecurityNo { get; set; }
+        public string DoctorName { get; set; }
+        public string PreferredComm { get; set; }
+        public string CommLanguage { get; set; }
+        public string PreferredPronouns { get; set; }
+        public string SexualOrientation { get; set; }
+        public string Active { get; set; }
+        public string DriversLicense { get; set; }
+        public string DriversLicenseState { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Race { get; set; }
+        public string Ethnicity { get; set; }
+    }
+}
---
+++ ChiroCloud_POC/Models/chirocloudContext.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace ChiroCloud_POC.Models
+{
+    public partial class chirocloudContext : DbContext
+    {
+        public chirocloudContext()
+        {
+        }
+
+        public chirocloudContext(DbContextOptions<chirocloudContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Patient> Patient { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Using dependency injection in Startup to handle conn string
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>(entity =>
+            {
+                entity.ToTable("patient");
+
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Active)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.CommLanguage)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("date");
+
+                entity.Property(e => e.DoctorName)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.DriversLicense)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.DriversLicenseState)
+                    .HasColumnType("varchar(2)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.EmployerId).HasColumnType("int(11)");
+
+                entity.Property(e => e.Ethnicity)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Gender)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.MaritalStatus)
+                    .HasColumnType("varchar(20)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NameFirst)
+                    .HasColumnType("varchar(30)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NameLast)
+                    .HasColumnType("varchar(30)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NameMiddle)
+                    .HasColumnType("varchar(30)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NamePreferred)
+                    .HasColumnType("varchar(30)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NamePrefix)
+                    .HasColumnType("varchar(15)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.NameSuffix)
+                    .HasColumnType("varchar(15)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.PreferredComm)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.PreferredPronouns)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Race)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Sex)
+                    .HasColumnType("varchar(10)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.SexualOrientation)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.SocialSecurityNo)
+                    .HasColumnType("varchar(9)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
